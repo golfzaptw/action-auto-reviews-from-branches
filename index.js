5dc62e3bc4fc377ca0e62e89265e12eb13c1bb44
Patawee Boonsongsri <psgolf16@gmail.com>
2019-12-04 14:49:30 +0700

    Improve script

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 
 try {
+  // Core Input
   const githubToken = core.getInput('GITHUB_TOKEN')
   const eventType = core.getInput('EVENT_TYPE')
   const branch = core.getInput('BRANCHES')
   const message = core.getInput('MESSAGE')
   const author = core.getInput('AUTHOR')
+  // Github context
   const headAuthor = github.context.actor
   const pullRequest = github.context.payload.pull_request
   const prNumber = pullRequest.number
   const headBranch = pullRequest.head.ref
+  // Regex check
   const resultBranch = new RegExp(branch, 'g').test(headBranch)
   const resultAuthor = new RegExp(`^${author}$`, 'g').test(headAuthor)
 
@@ -34,7 +37,7 @@ try {
   }
 
   if (eventType === 'APPROVE' && !message) {
-    body = '🏳️‍🌈Success approve. Enjoy 🙏.'
+    body = 'Success approve. Enjoy 🏳️‍🌈🎉.'
   } else {
     body = message
   }
@@ -47,7 +50,7 @@ try {
       eventType: eventType,
     })
   } else {
-    core.warning(`Branch not found at input`)
+    core.warning(`BRANCHES or AUTHOR not found at input`)
   }
 } catch (err) {
   core.error(err)
@@ -65,7 +68,7 @@ async function pullRequestReviews({token, prNumber, message, eventType}) {
       event: eventType,
     })
     .then(() => {
-      core.info(`Done.Please see at pull_request #${prNumber}`)
+      core.info(`Done. Please see at pull_request #${prNumber} 🎉`)
     })
     .catch(err => {
       core.error(err)
